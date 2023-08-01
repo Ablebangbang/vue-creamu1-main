09f0f1c30758ea187a77eae043e32f6c4cb535c1
CPBear7777 <129512908+CPBear7777@users.noreply.github.com>
2023-08-01 17:58:33 +0800

    Combine Carts

---
+++ Cart.vue
@@ -0,0 +1,12 @@
+<template>
+  <br />
+  <br />
+  <br />
+  <CtmizedPTable></CtmizedPTable>
+</template>
+
+<script setup>
+import CtmizedPTable from "../../components/CtmizedPTableInCart.vue";
+</script>
+
+<style scope></style>
